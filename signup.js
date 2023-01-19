39db8a21d3570a67347d7a218f43780c62618f64
akank-shan <86820759+akank-shan@users.noreply.github.com>
2023-01-19 17:19:07 +0530

    Update signup.js

--- signup.js
+++ signup.js
@@ -4,64 +4,26 @@ const login_input = document.getElementById('login_input');
 
 const loginErrBox = document.getElementById('loginErrBox');
 
-//login_input.addEventListener('input', ValidateEmail);
 
 const pswd_input = document.getElementById('pswd_input');
 
 const pswBox = document.getElementById('pswBox');
 
-// //pswd_input.addEventListener('input', ValidatePass);
 
 const user_input = document.getElementById('user_input');
 
 const userBox = document.getElementById('userBox');
-
-//user_input.addEventListener('input', ValidateUserName);
-
 
 
 const bio_input = document.getElementById('bio_input');
 
 const bioBox = document.getElementById('bioBox');
 
-//bio_input.addEventListener('input', ValidateBio);
 
 const cpswd_input = document.getElementById('cpswd_input');
 
 const cpswBox = document.getElementById('cpswBox');
 
-//cpswd_input.addEventListener('input', ValidateConfirmPass);
-
-
-// function ValidateEmail(mail) {
-//     console.log(mail.target.value);
-//     const value = mail.target.value;
-
-//     loginErrBox.style.display = "none";
-
-//     if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value)) {
-//         loginErrBox.style.display = "block";
-//     }
-
-//     // if(login_input == 0){
-//     //     alert("This field is required!!!");
-//     //     return false
-//     // }
-
-// }
-
-// function ValidatePass(pass) {
-//     console.log(pass.target.value);
-//     const value = pass.target.value;
-
-//     pswBox.style.display = "none";
-
-//     if (!/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{3,}/.test(value)) {
-//         pswBox.style.display = "block";
-
-//     }
-
-// }
 
 
 function redirect(pass) {
